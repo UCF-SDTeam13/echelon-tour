74acdd10f11053804bcd715544bf2bf28b9a73c1
Lisa Armenteros <lisaarmenteros@gmail.com>
2019-09-19 18:00:13 -0400

    Minor Changes
    Minor tweaks

--- Assets/Scripts/Testing/Login.cs
+++ Assets/Scripts/Testing/Login.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-using System.Collections.Generic;
+
 
 using Amazon;
 using Amazon.Runtime;
@@ -12,7 +12,11 @@ using Amazon.CognitoSync.SyncManager;
 
 public class SyncClient : MonoBehaviour
 {
-    public GameObject LoginUI;
+    public const string AppClientID = "59a98gf8s34jdqb90l07epsogs";
+    public const string UserPoolID = "us-east-2_JX78GvZOE";
+    public const string UserPoolName = "USERPOOLID";
+    RegionEndpoint CognitoIdentityRegion = RegionEndpoint.USEast1;
+
     string username;
     string password;
     Dataset playerInfo;
@@ -28,26 +32,10 @@ public class SyncClient : MonoBehaviour
         AWSConfigs.LoggingConfig.LogTo = LoggingOptions.UnityLogger;
 
         // Identity pool ID found under AWS >> Edit identity pool
-        credentials = new CognitoAWSCredentials("us-east-2:7d10094b-a03d-4641-bb01-0c077a64df98", RegionEndpoint.USEast1);
-        syncManager = new CognitoSyncManager(credentials, RegionEndpoint.USEast1);
-        playerInfo = syncManager.OpenOrCreateDataset("playerInfo");
+        CognitoAWSCredentials credentials = new CognitoAWSCredentials(
+            "us-east-2_JX78GvZOE", // IdentityPoolID
+            RegionEndpoint.USEast1) // Region
+        ;
     
-    }
-
-    public void ChangeUsername(string newUsername)
-    {
-        username = newUsername;
-        playerInfo.Put("username", newUsername);
-    }
-
-    public void ChangePassword(string newPassword)
-    {
-        password = newPassword;
-        playerInfo.Put("password", newPassword);
-    }
-
-    public void Synchronize()
-    {
-        playerInfo.SynchronizeOnConnectivity();
     }
 }
