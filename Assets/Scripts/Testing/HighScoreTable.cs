03574c5ab59c4338f7db2729abafcfd7073e7ede
Lisa Armenteros <lisaarmenteros@gmail.com>
2019-09-19 19:26:16 -0400

    New GameUI Scene/Scripts
    Made a UI for starting to create game UI elements such as the "End Results" board and the live leaderboard.
    
    Started messing arround with "End Results" table and some scripting.

---
+++ Assets/Scripts/Testing/HighScoreTable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreTable : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Transform entryContainter;
+    private Transform entryTemplate;
+    private void Awake()
+    {
+        entryContainter = transform.Find("HighScoreEntryContainer");
+        entryTemplate = entryContainter.Find("HighScoreEntryTemplate");
+        
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 20f;
+        for (int i = 0; i < 10; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainter);
+            RectTransform entryReactTransform = entryTransform.GetComponent<RectTransform>();
+            entryReactTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+        }
+    }
+}
