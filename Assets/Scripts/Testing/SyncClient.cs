5eb4c06aa936fcbd59f4d80174d30abbf55dbd1b
Lisa Armenteros <lisaarmenteros@gmail.com>
2019-09-19 16:01:09 -0400

    Login Page/Messing with Cognito
    Created a login page. Currently messing arroung with AWS Cognito.

---
+++ Assets/Scripts/Testing/SyncClient.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections.Generic;
+
+using Amazon;
+using Amazon.Runtime;
+using Amazon.CognitoIdentity;
+using Amazon.CognitoSync;
+using Amazon.CognitoSync.SyncManager;
+
+public class SyncClient : MonoBehaviour
+{
+Dataset playerInfo;
+CognitoSyncManager syncManager;
+CognitoAWSCredentials credentials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityInitializer.AttachToGameObject(this.gameObject);
+
+        // Remove if you want to build on IOS device.
+        AWSConfigs.LoggingConfig.LogTo = LoggingOptions.UnityLogger;
+
+        // Identity pool ID found under AWS >> Edit identity pool
+        credentials = new CognitoAWSCredentials("us-east-2:7d10094b-a03d-4641-bb01-0c077a64df98", RegionEndpoint.USEast1);
+        syncManager = new CognitoSyncManager(credentials, RegionEndpoint.USEast1);
+        playerInfo = syncManager.OpenOrCreateDataset("playerInfo");
+    
+    }
+}
