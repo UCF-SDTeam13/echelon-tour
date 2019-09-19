ceae80ea944f87833ae4f96c9cc2ef4560ca79ea
Lisa Armenteros <lisaarmenteros@gmail.com>
2019-09-18 20:41:49 -0400

    Revert "Revert "Basic UI Implementation""
    This reverts commit 01f8fc96990d559192e516565f3cd43804bf6254.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("BikeAnimationTestScene");
+    }
+
+    public void BikeTest()
+    {
+        SceneManager.LoadScene("HardwareTestScene");
+    }
+
+}
