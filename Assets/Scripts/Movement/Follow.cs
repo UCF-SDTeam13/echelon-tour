5a69fbf3fe6e4a2d9b4aa1710036dba88acbcfa9
wlam03 <wlam1394@yahoo.com>
2019-09-26 19:58:28 -0400

    Prefabs/Animations/Movement
    Prefabs are adjusted to work with animations (no more turning around). New animations added. Movement added to move played around a circuit.

---
+++ Assets/Scripts/Movement/Follow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField] private GameObject target = null;
+
+    public float speed = .05f;
+
+    private Tracker tracker;
+    private void Start()
+    {
+        tracker = GetComponent<Tracker>();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+
+        Vector3 targetDirection = target.transform.position - transform.position;
+        if (targetDirection != Vector3.zero)
+        {
+            //transform.rotation = Quaternion.LookRotation(targetDirection);
+
+            Vector3 lookAhead = tracker.target.position + tracker.target.forward;
+            float angle = Vector3.Angle(lookAhead, transform.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), angle * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("failed");
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/NewTracker.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class Tracker : MonoBehaviour
+{
+    // Reference to the circuit it should follow
+    public Circuit circuit = null;
+
+    // Offset ahead that the object should aim for
+    [SerializeField] private float lookAheadForTargetOffset = 5;
+
+    // Multiplier to adjust distance ahead that is based off speed
+    [SerializeField] private float lookAheadForTargetFactor = .1f;
+
+    // Offset ahead for speed adjustments (used with lerp for the target's rotation)
+    [SerializeField] private float lookAheadForSpeedOffset = 10f;
+
+    // Multiplier to adjust distance for speed
+    [SerializeField] private float lookAheadForSpeedFactor = .2f;
+
+    // Target for object
+    public Transform target;
+
+    // Possible variables for animations
+    public Vector3 targetPosition;
+    public Vector3 targetForward;
+
+    [SerializeField] private float speed;
+    [SerializeField] private int index = 0;
+
+    public Circuit.RoutePoint progressPoint;
+    private float progressDistance;
+    private Vector3 lastPosition;
+
+    private void Start()
+    {
+        Reset();
+    }
+
+    // Makes sure the progress distance is 0
+    public void Reset()
+    {
+        progressDistance = 0;
+    }
+
+    private void Update()
+    {
+        if (Time.deltaTime > 0)
+        {
+            speed = Mathf.Lerp(speed, (lastPosition - transform.position).magnitude / Time.deltaTime,
+                               Time.deltaTime);
+        }
+
+        // Change the position of the target in front of the object
+        target.position =
+            circuit.GetRoutePoint(progressDistance + lookAheadForTargetOffset + lookAheadForTargetFactor * speed, index)
+                .position;
+
+        // Change the rotation of the target in front of the object
+        target.rotation =
+            Quaternion.LookRotation(
+                circuit.GetRoutePoint(progressDistance + lookAheadForSpeedOffset + lookAheadForSpeedFactor * speed, index)
+                       .direction);
+
+        targetPosition = target.position;
+        targetForward = target.position + target.forward;
+
+        progressPoint = circuit.GetRoutePoint(progressDistance, index);
+        Vector3 progressDelta = progressPoint.position - transform.position;
+        if (Vector3.Dot(progressDelta, progressPoint.direction) < 0)
+        {
+            progressDistance += progressDelta.magnitude * 0.5f;
+        }
+
+        lastPosition = transform.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            // Draw a line to the target front position
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, target.position);
+
+            // Draw a line for the look ahead
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(target.position, target.position + target.forward);
+        }
+    }
+}
