e0890da0c4a758007a36a689e698b96fd8b245cc
Lisa Armenteros <lisaarmenteros@gmail.com>
2019-10-13 20:17:31 -0400

    Loading Bar/ Aspect Ratio
    Added a basic loading bar and did some scaling for different devices

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    public void LoadLevel(string name)
+    {
+        StartCoroutine(LoadAsynchronously(name));
+    }
+
+    IEnumerator LoadAsynchronously (string name)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+
+}
