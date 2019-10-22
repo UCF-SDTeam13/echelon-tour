9c62eb9009c6957c6ed7d1c6de9a2e65380b70d4
Lisa Armenteros <lisaarmenteros@gmail.com>
2019-10-22 19:33:17 -0400

    UI Changes/Scaling

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Options : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
