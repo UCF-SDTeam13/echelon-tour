3a42071555ed54bde8ec58f3cf31637c4f6ccc8a
Lisa Armenteros <lisaarmenteros@gmail.com>
2019-10-24 21:14:32 -0400

    Preload Created

---
+++ Assets/Loadgame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadgame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
