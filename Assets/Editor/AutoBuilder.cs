18aae781ff14fff14f814e673b86be5e5bf00969
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2019-11-21 19:51:01 -0500

    Add AutoBuilder for Potential CI / CD

---
+++ Assets/Editor/AutoBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+class Autobuilder
+{
+    [MenuItem("AutoBuilder/Android")]
+    public static void PerformAndroidBuild()
+    {
+        string[] scenes = { "Assets/Scenes/NetworkTestScene.unity" };
+
+        string buildPath = "../../../Build/Android";
+
+        // Create build folder if it does not exist
+        //Directory.CreateDirectory(buildPath);
+
+        BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.Android, BuildOptions.Development);
+    }
+}
