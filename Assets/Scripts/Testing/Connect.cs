6c0d3360ada8e13ebce5a22e8b03d5ec293561a6
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2019-11-21 19:54:54 -0500

    Change Connect / Login to Use API

--- Assets/Scripts/Testing/Connect.cs
+++ Assets/Scripts/Testing/Connect.cs
@@ -9,11 +9,6 @@ public class Connect : MonoBehaviour
 {
     private HttpClient client = new HttpClient();
     RealTimeClient rc;
-    string gameSessionId = "";
-    string playerSessionId = "";
-    string ipAddress = "";
-    string port = "";
-    string message = "";
     Ping east1;
     Ping east2;
 
@@ -23,7 +18,7 @@ public class Connect : MonoBehaviour
     int pingEast2 = 9999;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         client = new HttpClient();
         ConnectClient();
@@ -33,68 +28,52 @@ public class Connect : MonoBehaviour
 
     async void ConnectClient()
     {
-        BLEDebug.LogInfo("Getting Game Session");
-        Dictionary<string, string> param = new Dictionary<string, string>();
+        await API.Instance.Login("test", "password");
+        await API.Instance.CreateMatchmakingTicket();
+        do
+        {
+            BLEDebug.LogInfo("Checking Matchmaking Ticket Status");
+            await API.Instance.CheckMatchmakingTicketStatus();
+        } while (API.Instance.Status == "SEARCHING" || API.Instance.Status == "PLACING");
+        if (API.Instance.Status == "COMPLETED")
+        {
+            int iPort = 1900;
 
-        HttpContent req = FlatJSON.SerializeContent(param);
-        HttpResponseMessage result = await client.PostAsync("https://iddz0upx1d.execute-api.us-east-2.amazonaws.com/Prod/test/game/session", req);
+            Int32.TryParse(API.Instance.Port, out iPort);
+            rc = new RealTimeClient(API.Instance.IpAddress, iPort, 33400, ConnectionType.RT_OVER_WEBSOCKET, API.Instance.PlayerSessionId, new byte[0]);
 
-        string body = await result.Content.ReadAsStringAsync();
-        BLEDebug.LogInfo("GS" + body);
-        Dictionary<string, string> response = FlatJSON.Deserialize(body);
+        }
 
-        response.TryGetValue("message", out message);
-        response.TryGetValue("GameSessionId", out gameSessionId);
-        BLEDebug.LogInfo(gameSessionId);
-
-        BLEDebug.LogInfo("Getting Player Session");
-        param = new Dictionary<string, string>();
-        param.Add("GameSessionId", gameSessionId);
-
-        req = FlatJSON.SerializeContent(param);
-        result = await client.PostAsync("https://iddz0upx1d.execute-api.us-east-2.amazonaws.com/Prod/test/player/session", req);
-        req.Dispose();
-
-        body = await result.Content.ReadAsStringAsync();
-        BLEDebug.LogInfo("PS" + body);
-        response = FlatJSON.Deserialize(body);
-
-        response.TryGetValue("message", out message);
-        response.TryGetValue("PlayerSessionId", out playerSessionId);
-        response.TryGetValue("IpAddress", out ipAddress);
-        response.TryGetValue("Port", out port);
-        BLEDebug.LogInfo(playerSessionId);
-
-        int iPort = 1900;
-
-        Int32.TryParse(port, out iPort);
-        BLEDebug.LogInfo(ipAddress);
-        rc = new RealTimeClient(ipAddress, iPort, 33400, ConnectionType.RT_OVER_WEBSOCKET, playerSessionId, new byte[0]);
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         if (rc != null && rc.IsConnected())
         {
             //BLEDebug.LogInfo("Connection Status:" + rc.IsConnected());
-            //rc.SendMessage(DeliveryIntent.Reliable, "Hello, GameLift!");
         }
         timer += Time.deltaTime;
 
         if (east1.isDone)
         {
             pingEast1 = east1.time;
-            BLEDebug.LogInfo("East1 Ping" + pingEast1);
+
         }
         if (east2.isDone)
         {
             pingEast2 = east2.time;
-            BLEDebug.LogInfo("East2 Ping" + pingEast2);
-        }
 
+        }
         if (timer >= pingInterval)
         {
+            if (rc != null && rc.IsConnected())
+            {
+                BLEDebug.LogInfo("Connection Status:" + rc.IsConnected());
+                rc.UpdateStats(0, 0, 1, 2, 3, 1, 2, 3);
+            }
+            //BLEDebug.LogInfo("East1 Ping" + pingEast1);
+            //BLEDebug.LogInfo("East2 Ping" + pingEast2);
             east1 = new Ping("3.208.0.0");
             east2 = new Ping("3.14.0.0");
             timer = 0.0f;
