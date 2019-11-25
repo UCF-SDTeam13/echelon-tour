9ef17f63c9febdca0be04de0dc0d1923eff995d7
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2019-11-24 19:54:13 -0500

    Add Bluetooth Connection UI

---
+++ Assets/Scripts/UI/BluetoothConnectUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BluetoothConnectUI : MonoBehaviour
+{
+    public Button prefabButton;
+    public GameObject scrollContent;
+    HashSet<string> bikesListed = new HashSet<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bikesListed.Count != Bike.Instance.Matches.Count)
+        {
+            BLEDebug.LogInfo("BLE Button Added");
+            HashSet<string> newBikes = new HashSet<string>(Bike.Instance.Matches);
+            newBikes.ExceptWith(bikesListed);
+
+            foreach (string name in newBikes)
+            {
+                Button b = Object.Instantiate(prefabButton, Vector3.zero, Quaternion.identity);
+                RectTransform r = b.GetComponent<RectTransform>();
+                r.SetParent(scrollContent.GetComponent<RectTransform>());
+                b.GetComponentInChildren<Text>().text = name;
+                bikesListed.Add(name);
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        BLEPlugin.Instance.RequestEnableBLE();
+        BLEPlugin.Instance.Scan();
+    }
+
+    void OnDisable()
+    {
+        BLEPlugin.Instance.StopScan();
+    }
+}
