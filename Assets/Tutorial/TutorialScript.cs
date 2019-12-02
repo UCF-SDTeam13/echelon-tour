194c1119aee7999bb41fa273d7373ae7a2554637
wlam03 <wlam1394@yahoo.com>
2019-12-02 11:49:29 -0500

    Creation of Tutorial and added to 3 different scenes

---
+++ Assets/Tutorial/TutorialScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    public GameObject tutorial1;
+    public GameObject tutorial2;
+    public GameObject tutorial3;
+    public GameObject tutorial4;
+
+    public int oldIndex = 0;
+    public int newIndex = 0;
+
+    public GameObject[] tutorialArray;
+
+    public void Awake()
+    {
+        tutorialArray = new GameObject[] { tutorial1, tutorial2, tutorial3, tutorial4 };
+    }
+
+    public void LeftButtonChange()
+    {
+        newIndex = Mod(oldIndex - 1, 4);
+        tutorialArray[newIndex].SetActive(true);
+        tutorialArray[oldIndex].SetActive(false);
+        oldIndex = newIndex;
+    }
+
+    public void RightButtonChange()
+    {
+        newIndex = Mod(oldIndex + 1, 4);
+        tutorialArray[newIndex].SetActive(true);
+        tutorialArray[oldIndex].SetActive(false);
+        oldIndex = newIndex;
+    }
+
+    private int Mod(int a, int b)
+    {
+        return (a % b + b) % b;
+    }
+}
